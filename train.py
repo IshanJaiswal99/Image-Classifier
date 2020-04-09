652358a10b24ec6c5e85f39e38c2ce3081f0c948
Ishan Jaiswal <ishanjaiswal99@gmail.com>
2020-04-09 13:16:21 +0530

    feat: train.py train function complete

--- train.py
+++ train.py
@@ -68,3 +68,77 @@ def Createtransforms(data_dir):
     return dataset_sizes, loader, data_sets
 
 
+def build_train_model(option, dataset_sizes, loader, data_sets):
+    
+    #model = torchvision.models.vgg16(pretrained=True)
+    model = eval("torchvision.models.{}(pretrained=True)".format(option['arch']))
+    #Freezing parameters
+    for params in model.parameters():
+        params.requires_grad = False
+    #Define Classifier
+    model.classifier = nn.Sequential(collections.OrderedDict([
+        ('fc1', nn.Linear(25088, 4096, bias=True)),
+        ('relu1', nn.ReLU()),
+        ('dropout1', nn.Dropout(p=0.5)),
+        ('fc2', nn.Linear(4096, option['hidden_units'], bias=True)),
+        ('relu2', nn.ReLU()),
+        ('dropout1', nn.Dropout(p=0.25)),
+        ('fc3', nn.Linear(option['hidden_units'], 102, bias=True)),
+        ('output', nn.LogSoftmax(dim=1))
+        ]))
+
+    if option['cuda']:
+        device =   torch.device("cuda:0")
+    else:
+        device =  torch.device("cpu")
+    #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    #Loss and Optimizer
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr= option['learning_rate'] )
+
+    since = time.time()
+    scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=4,gamma=0.1,last_epoch=-1)
+    epochs = option['epochs']
+    print_every = 5
+    steps = 0
+    
+    for epoch in range(epochs):
+        print('Epoch {}/{}'.format(epoch + 1, epochs))
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  
+            else:
+                model.eval()
+            running_loss = 0
+            running_corrects = 0
+            
+            for inputs, labels in loader[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                #Zero Parameter Gradients
+                optimizer.zero_grad()
+                
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs,labels)
+                    
+                    if phase == 'train':
+                        loss.backward() 
+                        optimizer.step()
+                    running_loss += loss.item()*inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            print('{} loss: {:.4f} Accu: {:.4f}'.format(phase,epoch_loss,epoch_acc))
+            best_acc = epoch_acc
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    save_checkpoint(model,data_sets['train'],option)
+    
+ 
